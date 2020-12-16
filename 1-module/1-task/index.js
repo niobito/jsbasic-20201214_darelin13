215fa697f685dd7613510b79f4b4e3a8a652827b
dmitriy.bashev <darelin13@gmail.com>
2020-12-16 18:08:32 +0300

    fixed comments

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,19 +4,18 @@
  * @returns {number}
  */
 function factorial(n) {
-  let counter = 0;
-  let factorial = 0;
   if (n === 0) {
-    factorial = 1;
-  } else {
-    while (counter < n) {
-      if (!factorial) {
-        factorial = n;
-      } else {
-        factorial = factorial * (n - counter);
-      }
-      ++counter;
+    return 1;
+  }
+
+  let factorial = 0;
+  while (n) {
+    if (!factorial) {
+      factorial = n;
+    } else {
+      factorial = factorial * n;
     }
+    --n;
   }
   return factorial;
 }
