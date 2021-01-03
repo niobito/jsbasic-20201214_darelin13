6dd4fd3504c3961339bac769eb88472510156c46
dmitrii_bashev <darelin13@gmail.com>
2021-01-03 03:23:34 +0300

    added tasks from forth block

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,19 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  function getFullName({ firstName, lastName }) {
+    return `${firstName} ${lastName}`;
+  }
+
+  const ul = document.createElement('ul');
+  let list = new DocumentFragment();
+
+  for (let i = 0; i < friends.length; ++i) {
+    const li = document.createElement('li');
+    li.append(getFullName(friends[i]));
+    list.append(li);
+  }
+
+  ul.append(list);
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,21 @@
  * @param {Element} table
  */
 function highlight(table) {
+  const rows = table.querySelectorAll('tbody > tr:nth-child(n+1)');
+  rows.forEach(item => {
+    const cell = item.querySelectorAll('td');
+
+    if (cell[3].dataset.available === undefined) {
+      item.setAttribute('hidden', 'true');
+    } else {
+      cell[3].dataset.available === 'true' ? item.classList.add('available') : item.classList.add('unavailable');
+    }
+
+    cell[2].textContent === 'm' ?
+      item.classList.add('male') : item.classList.add('female');
+
+    if (Number(cell[1].textContent) < 18) {
+      item.style.textDecoration = 'line-through';
+    }
+  });
 }
